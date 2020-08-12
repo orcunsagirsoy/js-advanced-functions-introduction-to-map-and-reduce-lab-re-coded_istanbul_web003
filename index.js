e08509a42a071201e1a2d63d9a9f77d506ffbc44
orcunsagirsoy <orcunsagirsoy@gmail.com>
2020-08-12 11:42:25 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,63 @@
-// Your code here
+function mapToNegativize(sourceArray){
+    let newArray = [];
+    for(let i = 0; i < sourceArray.length; i++){
+        newArray[i] = (sourceArray[i] * -1);
+        
+    }
+    return newArray;
+}
+
+function mapToNoChange(sourceArray){
+    let newArray = [];
+    for(let i = 0; i < sourceArray.length; i++){
+        newArray[i] = sourceArray[i];
+        
+    }
+    return newArray;
+}
+
+function mapToDouble(sourceArray){
+    let newArray = [];
+    for(let i = 0; i < sourceArray.length; i++){
+        newArray[i] = (sourceArray[i] * 2);
+        
+    }
+    return newArray;
+}
+
+function mapToSquare(sourceArray){
+    let newArray = [];
+    for(let i = 0; i < sourceArray.length; i++){
+        newArray[i] = (sourceArray[i] * sourceArray[i]);
+        
+    }
+    return newArray;
+}
+
+function reduceToTotal(sourceArray, startingPoint=0){
+    let sum = startingPoint;
+    for(let i = 0; i < sourceArray.length; i++){
+        sum += sourceArray[i];
+    }
+    return sum
+}
+
+function reduceToAllTrue(sourceArray){
+    for(let i = 0; i < sourceArray.length; i++){
+        if(sourceArray[i] == false){
+            return false
+        }
+        
+    }
+    return true
+}
+
+function reduceToAnyTrue(sourceArray){
+    for(let i = 0; i < sourceArray.length; i++){
+        if(sourceArray[i] == true){
+            return true
+        }
+        
+    }
+    return false
+}
